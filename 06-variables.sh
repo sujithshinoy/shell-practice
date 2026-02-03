a3607543bac401491bd6623f4187dee25508fe4d
sujithkumar <sujithshinoy@gmail.com>
2026-02-03 12:43:52 +0530

    actions

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 
-TIMESTAMP=$(date)
+START_TIME=$(date +%s)
 
-echo "Script executed at: $TIMESTAMP"
+echo "Script started at: $TIMESTAMP"
+
+sleep 10
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$((END_TIME -START_TIME))
+
+
+echo "Script executed at: $TOTAl_TIME"
