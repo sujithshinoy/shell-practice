0fc055a55eeffa2653394b1b6df47ed7fd5fbf3c
sujithkumar <sujithshinoy@gmail.com>
2026-02-03 13:55:28 +0530

    actions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -6,9 +6,9 @@ NUMBER=35
 if[$NUMBER -gt 50]; then
 echo "$NUMBER is greater than 50"
 else
-if[$NUMBER -lt 50]; then
+if[$NUMBER -lt 50]; 
 echo "$NUMBER is less than 50"
 else
-if[$NUMBER -eq 50]; then
+if[$NUMBER -eq 50]; 
 echo "$NUMBER is equal to 50"
 fi
