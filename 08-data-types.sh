0f9fa8f42a546eb0029ef0ac525b7693012bef12
sujithkumar <sujithshinoy@gmail.com>
2026-02-03 13:30:12 +0530

    actions

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -6,3 +6,7 @@ NUM2=200
 SUM=$((NUM1 + NUM2))
 
 echo "The sum of $NUM1 and $NUM2 is: $SUM"
+
+#Arrays example
+FRUITS=("Apple" "Banana" "Cherry" "Date")
+echo "Fruits in the array are: ${FRUITS[@]}"
