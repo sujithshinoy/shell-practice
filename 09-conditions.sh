54265c207e8b04149b23383a82db1fdd3ca99cc9
sujithkumar <sujithshinoy@gmail.com>
2026-02-03 13:59:21 +0530

    actions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -3,12 +3,12 @@
 
 NUMBER=35
 
-if[$NUMBER -gt 50]; then
-echo "$NUMBER is greater than 50"
-else
-if[$NUMBER -lt 50]; 
+if[ $NUMBER -gt 50 ]; then
+echo "Given number :$NUMBER is greater than 50"
+
+elif[ $NUMBER -lt 50 ]; 
 echo "$NUMBER is less than 50"
-else
-if[$NUMBER -eq 50]; 
-echo "$NUMBER is equal to 50"
+
+elif[ $NUMBER -eq 35 ]; 
+echo "$NUMBER is equal to 35"
 fi
