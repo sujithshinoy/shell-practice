e4c480cf75970e189782a2a974f8325a3917f782
sujithkumar <sujithshinoy@gmail.com>
2026-02-03 12:09:26 +0530

    actions

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 
-echo "please Enter your user name :: "
+echo "please Enter your username:: "
 read USER_NAME
 
-echo "User name is $USER_NAME"
+echo "Username is $USER_NAME"
+
+echo -s"please Enter Your Password"
