3b85e644e6afb882fdfddee95c4f7490d8df8f6b
sujithkumar <sujithshinoy@gmail.com>
2026-02-05 12:35:47 +0530

    actions

--- 11-functions.sh
+++ 11-functions.sh
@@ -5,18 +5,19 @@ USERID=$(id -u)
 
 
 if [ $USERID -ne 0 ]; then
-echo "please run the script as root user access"
-exit 1
+   echo "please run the script as root user access"
+   exit 1
 fi
 
 Validate()
 {
     if [ $1 -ne 0 ]; then
-    echo "$2 ... failed"
-    exit 1
-    elif 
-    echo "$2 ... successful"
-fi
+     echo "$2 ... failed"
+     exit 1
+
+    else
+       echo "$2 ... successful"
+    fi
 }
 
 
