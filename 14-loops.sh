d660c814b5b321f49fddb86512dacdfb1cd75d15
sujithkumar <sujithshinoy@gmail.com>
2026-02-06 12:52:19 +0530

    actions

---
+++ 14-loops.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#!/bin/bash
+
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+
+
+if [ $USERID -ne 0 ]; then
+   echo "please run the script as root user access" | tee -a $LOGS_FILE
+   exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+Validate()
+{
+    if [ $1 -ne 0 ]; then
+     echo "$2 ... failed" | tee -a $LOGS_FILE
+     exit 1
+
+    else
+       echo "$2 ... successful" | tee -a $LOGS_FILE
+    fi
+}
+
+for package in $@
+do
+
+dnf install $package -y &>> $LOGS_FILE
+Validate $? "Installing $package"
+
+done
+
